cb78949473a0bd37374fbf31e9df573560168097
Elle-Dorvil <rachelle.dorvil@yahoo.com>
2025-04-28 10:56:02 -0400

    created class, included conductors, getters setters, toString

--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -1,4 +1,68 @@
 package com.pluralsight;
 
 public class Transaction {
+    private String startDate;
+    private String endDate;
+    private String description;
+    private String vendor;
+    private double amount;
+
+public Transaction(String startDate, String endDate, String Description, String Vendor, double Amount) {
+    this.startDate = startDate;
+    this.endDate = endDate;
+    this.description = description;
+    this.vendor = vendor;
+    this.amount = amount;
+}
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "startDate='" + startDate + '\'' +
+                ", endDate='" + endDate + '\'' +
+                ", description='" + description + '\'' +
+                ", vendor='" + vendor + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
 }
